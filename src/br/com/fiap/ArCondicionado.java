6d474f542ffefd060b40c3553967e61324360bb8
guilherme de araujo <guiam2909@gmail.com>
2026-03-05 10:38:13 -0300

    finalizando pratica de metodos atributos, mocagem e instanciacoes

--- src/br/com/fiap/ArCondicionado.java
+++ src/br/com/fiap/ArCondicionado.java
@@ -1,4 +1,17 @@
 package br.com.fiap;
 
 public class ArCondicionado {
+    // atributos
+    public float temperatura;
+    public String modo;
+    // metodos
+    public void aumentarTemperatura(){
+        temperatura++;
+    }
+    public void diminuirTemperatura(){
+        temperatura--;
+    }
+    public void trocarModo(String novoModo){
+        modo = novoModo;
+    }
 }
--- src/br/com/fiap/UsaArCondicionado.java
+++ src/br/com/fiap/UsaArCondicionado.java
@@ -1,4 +1,15 @@
 package br.com.fiap;
 
 public class UsaArCondicionado {
+    public static void main(String[] args) {
+        // teste mocado (mock)
+        // declaracao e instanciacao do objeto da classe ArCondicionado
+        ArCondicionado arcondicionado = new ArCondicionado();
+        arcondicionado.temperatura = 39f;
+        arcondicionado.modo = "cold";
+        arcondicionado.aumentarTemperatura();
+        arcondicionado.diminuirTemperatura();
+        arcondicionado.trocarModo("wind");
+        System.out.println("Temperatura: " + arcondicionado.temperatura + " \nModo: " + arcondicionado.modo);
+    }
 }
